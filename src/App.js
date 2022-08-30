4ce279d519bd8fc17e0df30903166bfc9e2bef08
BerkantGC <berkant103x@hotmail.com>
2022-08-30 13:36:33 +0300

    links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+import './Styles/Main.scss';
+import {FiShare} from "react-icons/fi";
 
 function App() {
+  //console.log('current URL 👉️', window.location.href);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main-container'>
+        <div className='header-title'>
+          
+          <img alt='logo' src={logo} width={200} height={200}></img>
+          <h1 className='title'>YAŞAR ÜNİVERSİTESİ MOBIL UYGULAMA GELİŞTİRME TOPLULUĞU</h1>
+        </div>
+        <div className='all-links'>
+          <a target="_blank" rel="noopener noreferrer" href='https://docs.google.com/forms/d/e/1FAIpQLSeyAfk6DE91Jce7G0C1llMBkc-rXjCbumVjOxH84Wy1vCLlFQ/viewform'>
+            <div>
+            Kayıt Formu
+            </div>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/mug.yasar/'>
+            <div>
+            Instagram
+            </div>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/company/ya%C5%9Far-%C3%BCniversitesi-mobil-uygulama-geli%C5%9Ftirme-toplulu%C4%9Fu/'>
+            <div>
+            Linkedin
+            </div>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.youtube.com/channel/UCgj31r8y7gOfyDh0mGJKQqQ'>
+            <div>
+            Youtube
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
