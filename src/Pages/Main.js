3e277ae056103e1be73f29cc0b0e4e767965ef6d
BerkantGC <berkant103x@hotmail.com>
2022-09-06 20:05:32 +0300

    YU MUG

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -73,11 +73,6 @@ function Route(){
             Linkedin
             </div>
           </a>
-          <a target="_blank" rel="noopener noreferrer" href='https://discord.gg/HZ9fXWkasY'>
-            <div>
-            Discord
-            </div>
-          </a>
           <a target="_blank" rel="noopener noreferrer" href='https://www.youtube.com/channel/UCgj31r8y7gOfyDh0mGJKQqQ'>
             <div>
             Youtube
