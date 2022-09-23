4ddeca90df217cc0ddb8e7ea1163ec7a08b506f4
BerkantGC <berkant103x@hotmail.com>
2022-09-23 11:10:56 +0300

    More Responsive SlideMenu

--- src/Components/SlideMenu.jsx
+++ src/Components/SlideMenu.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 
 import "../Styles/SlideMenu.scss";
 import {FiMenu} from "react-icons/fi"
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const EachNavigation = (props) => {
     return(
@@ -13,8 +15,21 @@ const EachNavigation = (props) => {
 export default function(props){
     const navigate = useNavigate();
 
+    const sliderRef = useRef();
+
+    useEffect(()=>{
+        const closeSlider = (event) => {
+            if(sliderRef.current && !sliderRef.current.contains(event.target))
+            {
+                props.setMenuShown(false);
+            }
+        }
+        document.addEventListener("click", closeSlider);
+        return  () => document.body.removeEventListener("click", closeSlider);
+    }, [])
+
     return(
-        <div className={`${"slide-menu-container"} ${props.isMenuShown && "active"}`} >
+        <div ref={sliderRef} className={`${"slide-menu-container"} ${props.isMenuShown && "active"}`} >
         <FiMenu size={40} className="slide-menu-btn" onClick={()=>props.setMenuShown(!props.isMenuShown)}/>
             <div style={{marginBottom: 70}}></div>
             <div className="navigations">
