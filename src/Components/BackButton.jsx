13f03c50c1d06c0f6135b19a5ae2350b6c5d3cf8
BerkantGC <berkant103x@hotmail.com>
2022-09-26 21:33:31 +0300

    YUMUG

--- src/Components/BackButton.jsx
+++ src/Components/BackButton.jsx
@@ -1,5 +1,6 @@
 import "../Styles/BackButton.scss";
 import {HiHome} from "react-icons/hi"
+
 export default function({navigate}){
     return(
         <HiHome size={30} color="#291b53" className="back-btn-container" onClick={()=>navigate("/")}>
